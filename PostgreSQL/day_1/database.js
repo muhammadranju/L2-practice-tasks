dc11dd02ff4feb0d67e54f71db35bb7809cfd81e
Muhammad Ranju <mdranju23@gmail.com>
2025-05-13 22:04:16 +0600

    Add initial Express server setup with PostgreSQL integration
    - Created package.json with dependencies for Express and pg
    - Implemented server.js with routes for GET, POST, and PUT operations on users
    - Established connection to PostgreSQL database

---
+++ PostgreSQL/day_1/database.js
@@ -0,0 +1,22 @@
+// db.js
+const { Pool } = require("pg");
+
+// PostgreSQL config
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "admin123",
+  port: 5432,
+});
+
+// Optional: test connection
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error connecting to the database:", err);
+  }
+  console.log("Connected to PostgreSQL DB!");
+  release();
+});
+
+module.exports = pool;
---
+++ PostgreSQL/day_1/server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require("express");
+const app = express();
+const pool = require("./database");
+
+app.use(express.json());
+
+// Example route
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM person");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+app.post("/users", async (req, res) => {
+  try {
+    const { id, name, city } = req.body;
+    const result = await pool.query(
+      `INSERT INTO person VALUES(${id}, '${name}','${city}')`
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { name, city } = req.body;
+    const { id } = req.params;
+    const result = await pool.query(
+      `UPDATE person SET name='${name}', city='${city}' WHERE id=${id}`
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+const PORT = 3050;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
