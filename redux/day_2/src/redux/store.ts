55287709136f1e5326d583bd130b8585dcab2d3d
Muhammad Ranju <mdranju23@gmail.com>
2025-07-01 00:43:48 +0600

    feat: add Redux slices for counter, task, and user management with API integration

---
+++ redux/day_2/src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "./features/counter/counterSlice";
+import taskReducer from "./features/task/taskSlice";
+import userReducer from "./features/user/userSlice";
+import { baseApi } from "./api/baseApi";
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    todo: taskReducer,
+    user: userReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
